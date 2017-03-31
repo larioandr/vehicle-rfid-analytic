14bbaa91ad86d25df78fd4fadfbec53ea886f28f
larioandr <larioandr@gmail.com>
2017-03-31 21:58:14 +0300

    Adds commands and their encodings

--- model/epcstd.py
+++ model/epcstd.py
@@ -1,6 +1,11 @@
 from enum import Enum
 
 
+#
+#######################################################################
+# Data Types
+#######################################################################
+#
 class DivideRatio(Enum):
     DR_8 = ('0', 8.0, '8')
     DR_643 = ('1', 64.0/3, '64/3')
@@ -142,3 +147,122 @@ class CommandCode(Enum):
 
     def __str__(self):
         return self._string
+
+
+#
+#######################################################################
+# API for encoding basic types
+#######################################################################
+#
+def encode_bool(value):
+    return '1' if value else '0'
+
+
+def encode_int(value, n_bits):
+    value %= 2 ** n_bits
+    return "{:0{width}b}".format(value, width=n_bits)
+
+
+def encode_word(value):
+    return encode_int(value, 16)
+
+
+def encode_byte(value):
+    return encode_int(value, 8)
+
+
+def encode_ebv(value, first_block=True):
+    prefix = '0' if first_block else '1'
+    if value < 128:
+        return prefix + format(value, '07b')
+    else:
+        return encode_ebv(value >> 7, first_block=False) + \
+               encode_ebv(value % 128, first_block=first_block)
+
+
+#
+#######################################################################
+# Commands
+#######################################################################
+#
+class Command:
+    def __init__(self, code):
+        super().__init__()
+        self._code = code
+
+    @property
+    def code(self):
+        return self._code
+
+    def encode(self):
+        raise NotImplementedError
+
+    @property
+    def bitlen(self):
+        s = self.encode()
+        return len(s)
+
+
+class Query(Command):
+    def __init__(self, dr=DivideRatio.DR_8, m=TagEncoding.FM0, trext=False,
+                 sel=SelFlag.ALL, session=Session.S0, target=InventoryFlag.A,
+                 q=0, crc=0x00):
+        super().__init__(CommandCode.QUERY)
+        self.dr = dr
+        self.m = m
+        self.trext = trext
+        self.sel = sel
+        self.session = session
+        self.target = target
+        self.q = q
+        self.crc = crc
+
+    def encode(self):
+        return (self.code.code + self.dr.code + self.m.code +
+                encode_bool(self.trext) + self.sel.code + self.session.code +
+                self.target.code + encode_int(self.q, 4) +
+                encode_int(self.crc, 5))
+
+
+class QueryRep(Command):
+    def __init__(self, session=Session.S0):
+        super().__init__(CommandCode.QUERY_REP)
+        self.session = session
+
+    def encode(self):
+        return self.code.code + self.session.code
+
+
+class Ack(Command):
+    def __init__(self, rn):
+        super().__init__(CommandCode.ACK)
+        self.rn = rn
+
+    def encode(self):
+        return self.code.code + encode_int(self.rn, 16)
+
+
+class ReqRN(Command):
+    def __init__(self, rn=0x0000, crc=0x0000):
+        super().__init__(CommandCode.REQ_RN)
+        self.rn = rn
+        self.crc = crc
+
+    def encode(self):
+        return self.code.code + encode_word(self.rn) + encode_word(self.crc)
+
+
+class Read(Command):
+    def __init__(self, bank=MemoryBank.RESERVED, word_ptr=0, word_count=0,
+                 rn=0x0000, crc=0x0000):
+        super().__init__(CommandCode.READ)
+        self.bank = bank
+        self.word_ptr = word_ptr
+        self.word_count = word_count
+        self.rn = rn
+        self.crc = crc
+
+    def encode(self):
+        return (self.code.code + self.bank.code + encode_ebv(self.word_ptr) +
+                encode_byte(self.word_count) + encode_word(self.rn) +
+                encode_word(self.crc))
--- test/epcstd_test.py
+++ test/epcstd_test.py
@@ -57,3 +57,77 @@ class TestDataTypes(unittest.TestCase):
         self.assertEqual(epcstd.CommandCode.ACK.code, '01')
         self.assertEqual(epcstd.CommandCode.REQ_RN.code, '11000001')
         self.assertEqual(epcstd.CommandCode.READ.code, '11000010')
+
+
+class TestEncodingFunctions(unittest.TestCase):
+
+    def test_encode_bool(self):
+        self.assertEqual(epcstd.encode_bool(True), '1')
+        self.assertEqual(epcstd.encode_bool(False), '0')
+
+    def test_encode_int(self):
+        self.assertEqual(epcstd.encode_int(0, 4), '0000')
+        self.assertEqual(epcstd.encode_int(0xF, 4), '1111')
+        self.assertEqual(epcstd.encode_byte(0xA5), '10100101')
+        self.assertEqual(epcstd.encode_word(0xAB3C), '1010101100111100')
+
+    def test_ebv(self):
+        self.assertEqual(epcstd.encode_ebv(0), '00000000')
+        self.assertEqual(epcstd.encode_ebv(1), '00000001')
+        self.assertEqual(epcstd.encode_ebv(127), '01111111')
+        self.assertEqual(epcstd.encode_ebv(128), '1000000100000000')
+        self.assertEqual(epcstd.encode_ebv(16383), '1111111101111111')
+        self.assertEqual(epcstd.encode_ebv(16384), '100000011000000000000000')
+
+
+class TestCommands(unittest.TestCase):
+
+    def test_query_command_encoding(self):
+        cmd1 = epcstd.Query(dr=epcstd.DivideRatio.DR_8,
+                            m=epcstd.TagEncoding.FM0, trext=False,
+                            sel=epcstd.SelFlag.ALL,
+                            session=epcstd.Session.S0,
+                            target=epcstd.InventoryFlag.A, q=0,
+                            crc=0x00)
+        self.assertEqual(cmd1.encode(), '1000000000000000000000')
+        self.assertEqual(cmd1.bitlen, 22)
+        cmd2 = epcstd.Query(dr=epcstd.DivideRatio.DR_643,
+                            m=epcstd.TagEncoding.M8, trext=True,
+                            sel=epcstd.SelFlag.SEL,
+                            session=epcstd.Session.S3,
+                            target=epcstd.InventoryFlag.B, q=6,
+                            crc=0x0B)
+        self.assertEqual(cmd2.encode(), '1000111111111011001011')
+
+    def test_query_rep_command_encoding(self):
+        cmd1 = epcstd.QueryRep(session=epcstd.Session.S0)
+        self.assertEqual(cmd1.encode(), '0000')
+        self.assertEqual(cmd1.bitlen, 4)
+        cmd2 = epcstd.QueryRep(session=epcstd.Session.S3)
+        self.assertEqual(cmd2.encode(), '0011')
+
+    def test_ack_command_encoding(self):
+        cmd1 = epcstd.Ack(rn=0x0000)
+        self.assertEqual(cmd1.encode(), '010000000000000000')
+        self.assertEqual(cmd1.bitlen, 18)
+        cmd2 = epcstd.Ack(rn=0xFFFF)
+        self.assertEqual(cmd2.encode(), '011111111111111111')
+
+    def test_req_rn_command_encoding(self):
+        cmd1 = epcstd.ReqRN(rn=0x0000, crc=0x0000)
+        self.assertEqual(cmd1.encode(),
+                         '1100000100000000000000000000000000000000')
+        self.assertEqual(cmd1.bitlen, 40)
+        cmd2 = epcstd.ReqRN(rn=0xAAAA, crc=0x5555)
+        self.assertEqual(cmd2.encode(),
+                         '1100000110101010101010100101010101010101')
+
+    def test_read_command_encoding(self):
+        cmd1 = epcstd.Read(bank=epcstd.MemoryBank.RESERVED, word_ptr=0,
+                           word_count=0, rn=0x0000, crc=0x0000)
+        self.assertEqual(cmd1.encode(), '11000010' + '0' * 50)
+        self.assertEqual(cmd1.bitlen, 58)
+        cmd2 = epcstd.Read(bank=epcstd.MemoryBank.USER, word_ptr=0x80,
+                           word_count=255, rn=0xAAAA, crc=0x5555)
+        self.assertEqual(cmd2.encode(), '11000010' + '11' + '1000000100000000'
+                         + '1' * 8 + '1010' * 4 + '0101' * 4)
